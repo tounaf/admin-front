76e16948799a447461100bc55daf966c86eeb4b6
Fetraharinjatovo <fetra.harinjatovo@pulse.mg>
2025-07-03 16:23:02 +0300

    remove filtr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,5 +13,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes), provideClientHydration(withEventReplay()),
     provideAnimations(),
     provideHttpClient(),
+    provideHttpClient(withFetch()),
   ]
 };
